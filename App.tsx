52d7c42c66cce41cb4d748778699e7d57a8d4d19
Rami Elwan <ramielwan48@gmail.com>
2025-07-24 13:48:26 +0200

    reproduce

--- App.tsx
+++ App.tsx
@@ -1,27 +1,94 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import {
+  Button,
+  FlatList,
+  ListRenderItemInfo,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import Animated, { FadeIn, FadeOut } from 'react-native-reanimated';
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [data, setData] = useState(() => [1]);
+
+  const onPress = () => {
+    const firstItem = data[0];
+    setData(prev => [firstItem - 1, ...prev]);
+  };
 
   return (
-    <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
+    <SafeAreaView style={styles.screen}>
+      <Button title="press me" onPress={onPress} />
+      <FlatList
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={item => item.toString()}
+      />
+    </SafeAreaView>
+  );
+}
+
+function renderItem(props: ListRenderItemInfo<number>) {
+  return <ListItem {...props} />;
+}
+
+function ListItem({ item }: ListRenderItemInfo<number>) {
+  const [displayTrailingView, setDisplayTrailingView] = useState(false);
+
+  useEffect(() => {
+    if (!displayTrailingView) return;
+
+    const timerId = setTimeout(() => {
+      setDisplayTrailingView(false);
+    }, 5000);
+
+    return () => clearTimeout(timerId);
+  }, [displayTrailingView]);
+
+  return (
+    <View style={styles.listItem}>
+      <View style={styles.listLeading}>
+        <Text>{item}</Text>
+        <Button title="press me" onPress={() => setDisplayTrailingView(true)} />
+      </View>
+      {displayTrailingView ? (
+        <Animated.View
+          entering={FadeIn.duration(2000)}
+          exiting={FadeOut.duration(2000)}
+          style={styles.listTrailing}
+        />
+      ) : null}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  screen: {
     flex: 1,
+    margin: 12,
+  },
+  listItem: {
+    borderWidth: 1,
+    borderColor: 'black',
+    padding: 12,
+    borderRadius: 4,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  listLeading: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  listTrailing: {
+    backgroundColor: 'red',
+    width: 25,
+    height: 25,
+  },
+  text: {
+    fontSize: 16,
   },
 });
 
